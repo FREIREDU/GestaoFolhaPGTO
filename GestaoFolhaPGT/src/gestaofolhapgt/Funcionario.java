814604a58f7eea75bb24a608d0e2e2cfa5fd0d5b
Eduardo Gabriel Freire <duduagaguiar@gmail.com>
2026-02-19 16:08:28 -0300

    Add files via upload

---
+++ GestaoFolhaPGT/src/gestaofolhapgt/Funcionario.java
@@ -0,0 +1,42 @@
+package gestaofolhapgt;
+
+public abstract class Funcionario {
+
+    private String nome;
+    private String cpf;
+    protected double salarioBase;
+
+    public Funcionario(String nome, String cpf, double salarioBase) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.salarioBase = salarioBase;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    // 📤 Setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    public abstract double calcularPagamento();
+}
+
---
+++ GestaoFolhaPGT/src/gestaofolhapgt/GestaoFolhaPGT.java
@@ -0,0 +1,8 @@
+package gestaofolhapgt;
+
+public class GestaoFolhaPGT {
+
+    public static void main(String[] args) {
+    }
+    
+}
